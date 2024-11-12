ee7057c7416961bccbaa10ce0d13c235df05e55b
Nikichva <niki.22.12.00@gmail.com>
2024-11-12 02:02:49 +0100

    Complete task 2

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,17 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = images
+  .map(
+    (image) =>
+      `<li><img src="${image.url}" alt="${image.alt}" class="gallery-image"></li>`
+  )
+  .join("");
+
+// console.log(galleryMarkup);
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
