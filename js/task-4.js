bb18fdbef1a057b8e932f6767e27030624807507
Nikichva <niki.22.12.00@gmail.com>
2024-11-15 01:02:11 +0100

    Complete task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+const emailInput = form.elements["email"];
+const passwordInput = form.elements["password"];
 
+const dataBase = {};
+
+form.addEventListener("submit", (event) => {
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
+    event.preventDefault();
+    alert("All form fields must be filled in");
+  } else {
+    event.preventDefault();
+    dataBase.email = emailInput.value.trim();
+    dataBase.password = passwordInput.value.trim();
+    form.reset();
+    console.log(dataBase);
+  }
+});
